829eb6707f224fb5e3d4915cbb6c3da49b7e615d
Seongyeon <115054681+deepshadow25@users.noreply.github.com>
2023-03-06 10:51:22 +0900

    Create Preprocessing.py

---
+++ Image_Recognizing/Preprocessing.py
@@ -0,0 +1,8 @@
+label2index = {'seg0' : 0, 'seg1' : 1 , 'seg2' : 2}
+labels = list(label2index.keys())
+
+# Train data
+'''!mkdir resized
+!mkdir resized/seg0
+!mkdir resized/seg1
+!mkdir resized/seg2
